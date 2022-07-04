d3d37b5ac71881f751399e52702a7733c0603a0c
chukwudumebiughonu <plasdexcel14@gmail.com>
2022-07-04 13:15:18 +0100

    completing the front page of the website

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,14 @@
-import React, { useContext } from 'react';
-
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
+import { getCategories } from '../services';
 
-const categories =  [{name: 'Home', slug: 'home'}, { name: 'Topics', slug: 'topics'}, { name: 'News', slug: 'news'}, { name: 'Resources', slug: 'resources'}]
 const   Header= () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        getCategories()
+            .then((newCategories) => setCategories(newCategories))
+    }, []);
   return (
     <div className= 'container mx-auto px-10 mb-8'>
         <div className='border-b w-full inline-block border-blue-400 py-8'>
@@ -11,11 +16,8 @@ const   Header= () => {
                 <Link href='/'>
                     <span className='cursor-pointer font-bold text-4xl text-white'>
                         On Precision Medicine
-                    <img 
-                    alt='kkkk'
-                    src='https://www.istockphoto.com/photo/license-gm618067194-?utm_medium=organic&utm_source=google&utm_campaign=iptcurl'
-                    />
                     </span>
+
                 </Link>
             </div>
            <div className='hidden md:float-left md:contents'>
