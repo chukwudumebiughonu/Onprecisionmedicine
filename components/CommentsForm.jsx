a3a0db1053a0b64666e177263603025e0e5f30af
chukwudumebiughonu <plasdexcel14@gmail.com>
2023-01-30 19:13:24 +0100

    revisiting

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -116,7 +116,7 @@ const CommentsForm = ({ slug }) => {
   };
   return (
     <div className='bg-white shadow-lg rounded-lg p-8 pb-12 mb-8'>
-        <h3 className='text-xl mb-8 font-semibold border-b pb-4'>Leave a Reply
+        <h3 className='text-xl mb-8 font-semibold border-b pb-4'>Leave a Reply 🙏
         </h3>
         <div className='grid grid-cols-1 gap-4 mb-4'>
             {/* <textarea 
